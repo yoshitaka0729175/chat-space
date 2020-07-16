339022e5920835d842d108ae4f4fbb1b64e3ec50
yoshitaka0729175 <yoshitaka0729175@gmail.com>
2020-07-16 13:30:29 +0900

    メッセージ自動送信機能

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
   if ( message.image ) {
     let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
     return html;
   } else {
     let html =
-    `<div class="MessageBox">
+    `<div class="MessageBox" data-message-id=${message.id}>
       <div class="MessageInfo">
         <div class="MessageInfo__userName">
           ${message.user_name}
@@ -61,7 +62,8 @@ $(function(){
       $('.btn').prop('disabled', false);
     })
     .fail(function() {
-      alert('メッセージを送信できません')
+      alert('メッセージを送信に失敗しました')
+      $('.Form__submit').prop("disabled", false);
     });      
   });
 });
